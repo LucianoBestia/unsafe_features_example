5b3301afe0e11da0058119190bda596a5c33f5ef
Luciano <luciano.bestia@gmail.com>
2020-06-16 13:36:50 +0530

    doc-comment

--- library/src/read_lyrics_mod.rs
+++ library/src/read_lyrics_mod.rs
@@ -1,11 +1,13 @@
 cfg_if::cfg_if! {
     if #[cfg(feature = "unsafe_feature_read_song")] {
         pub fn read_song()->String{
-            let path = "sample_data/songs/basket_case_green_day.txt";
             let file_content;
-            // one idea how to force the existence of "unsafe feature":
-            // unsafe("unsafe_feature_read_song"){...}
+            /// every unsafe should have a doc-comment, to describe what it does
+            /// this unsafe reads from "sample_data/songs/basket_case_green_day.txt"
+            /// one idea how to force the existence of "unsafe feature":
+            ///  unsafe("unsafe_feature_read_song"){...}
             unsafe{
+                let path = "sample_data/songs/basket_case_green_day.txt";
                 // std::fs should be "unsafe"
                 // therefore the code must be under a namespace of "unsafe features"
                 file_content = std::fs::read_to_string(path).unwrap();
@@ -14,6 +16,7 @@ cfg_if::cfg_if! {
             file_content
         }
 }  else {
+    /// safe alternative
     pub fn read_song()->String {"no feature, no access.".to_string()}
 }
 }
